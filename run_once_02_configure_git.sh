0a02bf0c32432f99b981615a87a877af9e4e692a
SONE Hiroaki <sonehiro@gmail.com>
2026-09-19 12:50:06 +0900

    refactor: dotfiles管理をprivateリポジトリへ集約

--- run_once_02_configure_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# === Git のグローバル設定 ===
-
-# グローバル gitignore の設定
-git config --global core.excludesfile "${HOME}/.config/git/ignore"
-
-# 共有 git config の読み込み
-git config --global include.path "${HOME}/.config/git/config"
